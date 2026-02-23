f58aa3323581b90c234475dcadc0880eb49dd3f4
Carlos Juarez <cjuarezramos@gmail.com>
2026-02-23 17:24:25 -0600

    Ejemplos para presentaciones

---
+++ Recursos Complementarios/PenduloInvertido.m
@@ -0,0 +1,7 @@
+% Autor: Carlos Juárez
+% Fecha: 18/02/2026
+% Descripción: Programa para obtener modelo matemático del pendulo
+% invertido. Usando Toolbox Symbolic y Control
+
+% Definición de variables simbólicas
+syms Ms ms bs ls Is Fs xs theta
