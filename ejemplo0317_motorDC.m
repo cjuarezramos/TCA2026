31491de2317e3165fab681621b185da20c3c0cc0
Carlos Juárez <cjuarezramos@gmail.com>
2026-03-17 10:14:41 -0600

    ejemplo version 18

--- ejemplo0317_motorDC.m
+++ ejemplo0317_motorDC.m
@@ -9,3 +9,11 @@ Ka = 0.25;   % Constante de ganancia del amplificador (V/rad/s)
 Km = 0.25;   % Constante de ganancia del motor (V/rad/s)
 J = 0.00246; % Inercia del rotor (kg*m^2)
 B = 0.003026; % Coeficiente de fricción viscosa (N*m*s/rad)
+
+num = [Km/J/L];
+den = [1,(B*L+R*J)/J/L,(B*R+Ka*Km)/J/L];
+
+Gmotor = tf(num,den) % creacion de funcion de transferencia
+step(Gmotor)
+
+roots(den)
