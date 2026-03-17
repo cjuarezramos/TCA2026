28f0da6efc37b656e98f97f9ecda9cdfb986565f
Carlos Juárez <cjuarezramos@gmail.com>
2026-03-17 09:15:00 -0600

    datos de motor DC

---
+++ ejemplo0317_motorDC.m
@@ -0,0 +1,11 @@
+% Autor: Carlos Juarez
+% Fecha: 17 de marzo de 2026
+% Descripción: Controladores
+
+% Parámetros del sistema
+R = 2.27; % Resistencia (Ohm)
+L = 0.0047; % Inductancia (H)
+Ka = 0.25;   % Constante de ganancia del amplificador (V/rad/s)
+Km = 0.25;   % Constante de ganancia del motor (V/rad/s)
+J = 0.00246; % Inercia del rotor (kg*m^2)
+B = 0.003026; % Coeficiente de fricción viscosa (N*m*s/rad)
